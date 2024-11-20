af333cf14742ca3834d215bf1b15a141be38e6e8
Mohamed Bentaleb <mbentale@e1r6p2.1337.ma>
2024-11-20 11:31:49 +0100

    the header is done

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,26 @@
 /*   By: mbentale <mbentale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 15:17:22 by mbentale          #+#    #+#             */
-/*   Updated: 2024/11/19 10:16:00 by mbentale         ###   ########.fr       */
+/*   Updated: 2024/11/19 15:39:55 by mbentale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
+# include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
 
-char *get_next_line(int fd);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
 
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
